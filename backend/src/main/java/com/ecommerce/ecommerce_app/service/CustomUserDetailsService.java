dbb2f8bea7890cbe7ba5393f864789aab4e08b10
jemek27 <emajure27@gmail.com>
2025-04-16 14:11:29 +0200

    feat: DTO's

--- backend/src/main/java/com/ecommerce/ecommerce_app/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/ecommerce/ecommerce_app/service/CustomUserDetailsService.java
@@ -2,12 +2,17 @@ package com.ecommerce.ecommerce_app.service;
 
 import com.ecommerce.ecommerce_app.model.User;
 import com.ecommerce.ecommerce_app.model.Role;
+import com.ecommerce.ecommerce_app.dto.UserSummaryDTO;
 import com.ecommerce.ecommerce_app.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,18 +25,38 @@ public class CustomUserDetailsService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
+    public User getUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    public Optional<User> getUserById(Long userId) { return userRepository.findById(userId); }
+
+    public List<UserSummaryDTO> getAllUsers() {
+        return userRepository.findAll().stream().map(UserSummaryDTO::new).collect(Collectors.toList());
+    }
+
     public boolean userExists(String username) {
         return userRepository.existsByUsername(username);
     }
+    public boolean userExists(Long id) { return userRepository.existsById(id); }
 
     public void createUser(String username, String encodedPassword, Role role) {
         User user = new User(username, encodedPassword, role);
         userRepository.save(user);
     }
 
-    public User getUserById(Long userId) {
-        return userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public void updateUser(User user) { userRepository.save(user); }
+
+    public void updateUserRole(Long userId, Role role)  throws UsernameNotFoundException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        user.setRole(role);
+        userRepository.save(user);
     }
 }
 
